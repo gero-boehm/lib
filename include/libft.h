14325741e7a6714d4065a5b258347212b8273371
Gero Bohm <gbohm@1-D-26.42heilbronn.de>
2023-01-31 15:52:56 +0100

    added ft_split2 to header

--- include/libft.h
+++ include/libft.h
@@ -6,7 +6,7 @@
 /*   By: gbohm <gbohm@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/11 13:13:50 by gbohm             #+#    #+#             */
-/*   Updated: 2023/01/12 17:23:11 by gbohm            ###   ########.fr       */
+/*   Updated: 2023/01/31 15:52:34 by gbohm            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -70,5 +70,6 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
 int		ft_malloc2(size_t count, void **p);
 int		ft_calloc2(size_t count, size_t size, void **p);
 int		ft_extstr2(char **str, size_t amount);
+int		ft_split2(char const *str, char c, char ***arr);
 
 #endif
